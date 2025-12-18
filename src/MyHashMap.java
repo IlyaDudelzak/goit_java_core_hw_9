faa1d0d4cebeec729acc5a9069cdb7b5031a68f7
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2025-12-18 18:06:55 +0200

    Update MyHashMap.java

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,59 +1,113 @@
 import java.util.NoSuchElementException;
 
-public class MyHashMap {
-    private class HashKeyValuePair {
+public class MyHashMap<K, V> {
+    private class HashKeyValuePair<K, V> {
         int hashKey;
-        Object value;
+//        final K key;
+        V value;
 
-        public HashKeyValuePair(Object key, Object value){
+        HashKeyValuePair<K, V> next;
+
+        public HashKeyValuePair(K key, V value){
             this.hashKey = key.hashCode();
+//            this.key = key;
             this.value = value;
         }
-        public boolean isSameKey(Object key){
+        public boolean isSameKey(K key){
             return hashKey == key.hashCode();
         }
     }
 
-    MyArrayList<HashKeyValuePair> list;
+    private HashKeyValuePair[] elementData;
+    private int size;
 
-    private HashKeyValuePair getKVP(Object key){
-        for (int i = 0; i < list.size(); i++) {
-            HashKeyValuePair keyValuePair = list.get(i);
-            if(keyValuePair.isSameKey(key))
-                return keyValuePair;
-        }
-        throw new NoSuchElementException();
-    }
-    public boolean keyInTable(Object key){
-        boolean in = false;
-        for (int i = 0; i < list.size(); i++) {
-            HashKeyValuePair keyValuePair = list.get(i);
-            in = in || keyValuePair.isSameKey(key);
-        }
-        return in;
+    public MyHashMap(){
+        this(16);
     }
 
-    public void put(Object key, Object value){
-        if(!keyInTable(key)){
-            list.add(new HashKeyValuePair(key, value));
+    public MyHashMap(int size){
+        elementData = new HashKeyValuePair[size];
+        size = 0;
+    }
+
+    private int getIndex(K key){
+        return key.hashCode() & (elementData.length - 1);
+    }
+
+    private HashKeyValuePair<K, V> getKVPNoExc(K key){
+        HashKeyValuePair<K, V> node = elementData[getIndex(key)];
+        if(node == null){
+            return null;
+        }
+        while(true){
+            if (node.isSameKey(key)) {
+                return node;
+            }
+            if(node.next == null) {
+                return null;
+            }
+            node = node.next;
+        }
+    }
+
+    private HashKeyValuePair<K, V> getKVP(K key){
+        HashKeyValuePair<K, V> node = getKVPNoExc(key);
+        if(node == null){
+            throw new NoSuchElementException();
+        }
+        return null;
+    }
+
+    public boolean keyInTable(K key){
+        return getKVPNoExc(key) != null;
+    }
+
+    public void put(K key, V value){
+        int index = getIndex(key);
+        HashKeyValuePair<K, V> node = getKVPNoExc(key);
+        HashKeyValuePair<K, V> newNode = new HashKeyValuePair<K, V>(key, value);
+        if(node == null){
+            if(elementData[index] == null) {
+                elementData[index] = newNode;
+            } else {
+                node = elementData[index];
+                while(true){
+                    if(node.next == null) {
+                        node.next = newNode;
+                    }
+                    node = node.next;
+                }
+            }
         } else {
-            getKVP(key).value = value;
+            node.value = value;
+        }
+        size++;
+    }
+    public void remove(K key) {
+        int index = getIndex(key);
+        if(elementData[index] == null) { throw new NoSuchElementException(); }
+        size--;
+        if(elementData[index].next == null) { elementData[index] = null; return; }
+        HashKeyValuePair<K, V> node = getKVPNoExc(key);
+        while(true){
+            if(node.next == null) { throw new NoSuchElementException(); }
+            if(node.next.isSameKey(key)){
+                if(node.next.next == null){
+                    node.next = null;
+                } else {
+                    node.next = node.next.next;
+                }
+            }
         }
     }
-    public void remove(Object key){
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).isSameKey(key))
-                list.remove(i);
-        }
+    public void clear() {
+        elementData = new HashKeyValuePair[elementData.length];
+        size = 0;
     }
-    public void clear(){
-        list.clear();
+    public int size() {
+        return size;
     }
-    public int size(){
-        return list.size();
-    }
-    public Object get(Object key){
-        if (!keyInTable(key)) throw new NoSuchElementException();
+    public V get(K key){
         return getKVP(key).value;
     }
 }
